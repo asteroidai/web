ebea5f56139085f89675d57a7e7eb84233fa80c3
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2025-01-02 12:58:23 +0530

    removed logging tsparticles and the same key warning

--- src/Stars.tsx
+++ src/Stars.tsx
@@ -31,7 +31,7 @@ export default function Stars() {
   }, []);
 
   const particlesLoaded = async (container?: Container): Promise<void> => {
-    console.log(container);
+    // console.log(container);
   };
 
   const options: ISourceOptions = useMemo(
